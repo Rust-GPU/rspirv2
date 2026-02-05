7677fdf2cefb4f852e39641dcd12495d65c725c2
firestar99 <firestar99@vectorware.com>
2026-02-05 16:09:36 +0100

    literal: rename `LiteralInteger` to `LiteralConst`, representing `LiteralContextDependentNumber`

--- crates/grammar/src/operand/literal_const.rs
+++ crates/grammar/src/operand/literal_const.rs
@@ -1,31 +1,36 @@
 use crate::operand::Word;
 use smallvec::SmallVec;
 
-/// A literal integer as defined by SPIR-V spec. May represent any number of [`Word`]s, depending on the type of the
-/// constant.
+/// A `LiteralContextDependentNumber`, or `LiteralConst` for short since it's only used by `OpConstant` (and
+/// `OpSpecConstant`) instructions.
+///
+/// A number of content dependent length, as defined by SPIR-V spec. May represent any number of [`Word`]s, depending on
+/// the type of the constant.
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
-pub struct LiteralInteger(SmallVec<[Word; 2]>);
+pub struct LiteralConst(SmallVec<[Word; 2]>);
 
-impl From<u64> for LiteralInteger {
+pub type LiteralContextDependentNumber = LiteralConst;
+
+impl From<u64> for LiteralConst {
     #[inline]
     fn from(value: u64) -> Self {
-        LiteralInteger(SmallVec::from_slice(&[
+        LiteralConst(SmallVec::from_slice(&[
             Word(value as u32),
             Word((value >> 32) as u32),
         ]))
     }
 }
 
-impl From<u32> for LiteralInteger {
+impl From<u32> for LiteralConst {
     #[inline]
     fn from(value: u32) -> Self {
-        LiteralInteger(SmallVec::from_slice(&[Word(value)]))
+        LiteralConst(SmallVec::from_slice(&[Word(value)]))
     }
 }
 
 macro_rules! impl_cast {
     ($ty:ty => $to:ty) => {
-        impl From<$ty> for LiteralInteger {
+        impl From<$ty> for LiteralConst {
             #[inline]
             fn from(value: $ty) -> Self {
                 Self::from(value as $to)
@@ -43,7 +48,7 @@ impl_cast!(i64 => u64);
 
 macro_rules! impl_float {
     ($ty:ty) => {
-        impl From<$ty> for LiteralInteger {
+        impl From<$ty> for LiteralConst {
             #[inline]
             fn from(value: $ty) -> Self {
                 Self::from(value.to_bits())
--- crates/grammar/src/operand/mod.rs
+++ crates/grammar/src/operand/mod.rs
@@ -1,11 +1,11 @@
 mod id;
+mod literal_const;
 mod literal_float;
-mod literal_integer;
 mod literal_string;
 
 pub use id::*;
+pub use literal_const::*;
 pub use literal_float::*;
-pub use literal_integer::*;
 pub use literal_string::*;
 
 /// A 32bit SPIR-V Word
