7443395f2a712f8eb21cf8841ad55a739fb1fbf7
firestar99 <firestar99@vectorware.com>
2026-02-09 10:01:39 +0100

    encode: remove deps `thiserror` and `syn`

--- crates/grammar/src/binary/error.rs
+++ crates/grammar/src/binary/error.rs
@@ -1,46 +1,81 @@
 use std::fmt::{Debug, Display, Formatter};
 use std::string::FromUtf8Error;
-use thiserror::Error;
 
-#[derive(Clone, Error, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub enum DecodeError {
-    #[error(
-        "Implementation Limitation: The `LiteralConst` must be the last operand of an Instruction for parsing to \
-        function properly. See documentation of `LiteralConst` for details."
-    )]
     LiteralIntegerNotLastOperand,
     /// This error must be cheap to crate, it will be discarded when iterating an `InstructionReader`.
-    #[error(
-        "The Instruction at offset {inst_offset} with {op_len} param words tried to decode more Operants than \
-    were available"
-    )]
-    InstructionDecodePulledTooManyWords { inst_offset: usize, op_len: usize },
-    #[error("The fixed-size Instruction at offset {inst_offset} has extra Operants")]
-    InstructionWithAdditionalOperants { inst_offset: usize },
-    #[error(
-        "The variable-sized Instruction at offset {inst_offset} has an unexpected operand length {op_len} which \
-    was expected to be a multiple of {expected_multiple}"
-    )]
+    InstructionDecodePulledTooManyWords {
+        inst_offset: usize,
+        op_len: usize,
+    },
+    InstructionWithAdditionalOperants {
+        inst_offset: usize,
+    },
     InstructionWithMismatchedVariableOperants {
         inst_offset: usize,
         op_len: usize,
         expected_multiple: usize,
     },
-    #[error(
-        "The Instruction at offset {inst_offset} has a supposed length of {op_len} but the module only has \
-        {module_remaining} words remaining"
-    )]
     InstructionTooLong {
         inst_offset: usize,
         op_len: usize,
         module_remaining: usize,
     },
-    #[error("UTF-8 error: {0}")]
-    Utf8Error(#[from] FromUtf8Error),
+    Utf8Error(FromUtf8Error),
+}
+
+impl Display for DecodeError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DecodeError::LiteralIntegerNotLastOperand => write!(
+                f,
+                "Implementation Limitation: The `LiteralConst` must be the last operand of an Instruction for parsing \
+                 to function properly. See documentation of `LiteralConst` for details."
+            ),
+            DecodeError::InstructionDecodePulledTooManyWords {
+                inst_offset,
+                op_len,
+            } => write!(
+                f,
+                "The Instruction at offset {inst_offset} with {op_len} param words tried to decode more Operants than \
+                were available"
+            ),
+            DecodeError::InstructionWithAdditionalOperants { inst_offset } => write!(
+                f,
+                "The fixed-size Instruction at offset {inst_offset} has unexpected additional Operants"
+            ),
+            DecodeError::InstructionWithMismatchedVariableOperants {
+                inst_offset,
+                op_len,
+                expected_multiple,
+            } => write!(
+                f,
+                "The variable-sized Instruction at offset {inst_offset} has an unexpected operand length {op_len} \
+                which was expected to be a multiple of {expected_multiple}"
+            ),
+            DecodeError::InstructionTooLong {
+                inst_offset,
+                op_len,
+                module_remaining,
+            } => write!(
+                f,
+                "The Instruction at offset {inst_offset} has a supposed length of {op_len} but the module only has \
+                {module_remaining} words remaining"
+            ),
+            DecodeError::Utf8Error(inner) => write!(f, "UTF-8 error: {inner}"),
+        }
+    }
 }
 
 impl Debug for DecodeError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         Display::fmt(self, f)
+    }
+}
+
+impl From<FromUtf8Error> for DecodeError {
+    fn from(value: FromUtf8Error) -> Self {
+        Self::Utf8Error(value)
     }
 }
