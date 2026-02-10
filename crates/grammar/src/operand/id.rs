71419ea702123543e6cd66de48e51644d5405dba
firestar99 <firestar99@vectorware.com>
2026-02-10 16:28:55 +0100

    encode: impl Operand for `IdResult`

--- crates/grammar/src/operand/id.rs
+++ crates/grammar/src/operand/id.rs
@@ -11,6 +11,22 @@ use crate::operand::{Operand, OperandEncoding, Word};
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct IdResult(pub Word);
 
+impl Operand for IdResult {
+    const KIND: OperandKind = crate::core::operand_kinds::OPERAND_KIND_ID_RESULT;
+}
+
+impl OperandEncoding for IdResult {
+    const FIXED_LEN: Option<usize> = Some(1);
+
+    fn encode(&self, writer: &mut impl InstructionWriter) -> Result<(), EncodeError> {
+        writer.push(self.0)
+    }
+
+    fn decode(reader: &mut InstructionReader<'_>) -> Result<Self, DecodeError> {
+        Ok(Self(reader.pull()?))
+    }
+}
+
 macro_rules! id_ref {
     ($name:ident; $kind:expr; $docs:literal) => {
         #[doc = concat!("A `", stringify!($name), "` is a reference to a [`ResultId`] of another operation.")]
