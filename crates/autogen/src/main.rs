67812e40a0a00184d005c20bd7bc90ad61bdce76
firestar99 <firestar99@vectorware.com>
2026-03-06 12:48:21 +0100

    rayon autogen: parallelize grammar writing, 2.18s/1.03s

--- crates/autogen/src/main.rs
+++ crates/autogen/src/main.rs
@@ -1,7 +1,9 @@
 #![doc = include_str!("../README.md")]
 
+use rayon::prelude::*;
 use rspirv2_grammar::{PATH_GRAMMAR_CORE, PATH_GRAMMAR_DEBUG_PRINTF, PATH_GRAMMAR_GLSL_STD_450};
 use rspirv2_grammar_parser::codegen::{CodegenOptions, GrammarWriter, write_grammar};
+use rspirv2_grammar_parser::proc_macro2::TokenStream;
 use rspirv2_grammar_parser::quote::quote;
 use rspirv2_grammar_parser::timer::TimerPrintOnDrop;
 use std::path::Path;
@@ -13,22 +15,37 @@ pub fn main() -> anyhow::Result<()> {
     autogen()
 }
 
+pub fn path_to_core() -> TokenStream {
+    quote!(crate::core)
+}
+
 pub fn autogen() -> anyhow::Result<()> {
-    write_grammar(
-        GrammarWriter::new(Path::new(PATH_GRAMMAR_CRATE_SRC).join("core"))?,
-        &PATH_GRAMMAR_CORE.read()?.parse_grammar()?,
-        &CodegenOptions::new_core(),
-    )?;
-    let path_to_core = quote!(crate::core);
-    write_grammar(
-        GrammarWriter::new(Path::new(PATH_GRAMMAR_CRATE_SRC).join("glsl_std_450"))?,
-        &PATH_GRAMMAR_GLSL_STD_450.read()?.parse_grammar()?,
-        &CodegenOptions::new_ext_inst_set("Glsl", &path_to_core),
-    )?;
-    write_grammar(
-        GrammarWriter::new(Path::new(PATH_GRAMMAR_CRATE_SRC).join("debug_printf"))?,
-        &PATH_GRAMMAR_DEBUG_PRINTF.read()?.parse_grammar()?,
-        &CodegenOptions::new_ext_inst_set("DebugPrintf", &path_to_core),
-    )?;
+    [
+        || {
+            write_grammar(
+                GrammarWriter::new(Path::new(PATH_GRAMMAR_CRATE_SRC).join("core"))?,
+                &PATH_GRAMMAR_CORE.read()?.parse_grammar()?,
+                &CodegenOptions::new_core(),
+            )
+        },
+        || {
+            write_grammar(
+                GrammarWriter::new(Path::new(PATH_GRAMMAR_CRATE_SRC).join("glsl_std_450"))?,
+                &PATH_GRAMMAR_GLSL_STD_450.read()?.parse_grammar()?,
+                &CodegenOptions::new_ext_inst_set("Glsl", &path_to_core()),
+            )
+        },
+        || {
+            write_grammar(
+                GrammarWriter::new(Path::new(PATH_GRAMMAR_CRATE_SRC).join("debug_printf"))?,
+                &PATH_GRAMMAR_DEBUG_PRINTF.read()?.parse_grammar()?,
+                &CodegenOptions::new_ext_inst_set("DebugPrintf", &path_to_core()),
+            )
+        },
+    ]
+    .as_slice()
+    .par_iter()
+    .map(|f| f())
+    .collect::<anyhow::Result<()>>()?;
     Ok(())
 }
