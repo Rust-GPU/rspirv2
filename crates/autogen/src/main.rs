291a679540f580caa6bf15ce04e11fee534bb719
firestar99 <firestar99@vectorware.com>
2026-03-06 12:42:35 +0100

    autogen: add TimerPrintOnDrop

--- crates/autogen/src/main.rs
+++ crates/autogen/src/main.rs
@@ -3,11 +3,17 @@
 use rspirv2_grammar::{PATH_GRAMMAR_CORE, PATH_GRAMMAR_DEBUG_PRINTF, PATH_GRAMMAR_GLSL_STD_450};
 use rspirv2_grammar_parser::codegen::{CodegenOptions, GrammarWriter, write_grammar};
 use rspirv2_grammar_parser::quote::quote;
+use rspirv2_grammar_parser::timer::TimerPrintOnDrop;
 use std::path::Path;
 
 pub const PATH_GRAMMAR_CRATE_SRC: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../rspirv2/src/");
 
 pub fn main() -> anyhow::Result<()> {
+    let _timer = TimerPrintOnDrop::new("autogen");
+    autogen()
+}
+
+pub fn autogen() -> anyhow::Result<()> {
     write_grammar(
         GrammarWriter::new(Path::new(PATH_GRAMMAR_CRATE_SRC).join("core"))?,
         &PATH_GRAMMAR_CORE.read()?.parse_grammar()?,
