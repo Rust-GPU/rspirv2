2fde0476dc564f0508311db6ed0071fa9c1b650f
firestar99 <firestar99@vectorware.com>
2026-03-10 13:34:36 +0100

    example: add `rspirv-examples` for prototyping, move `debug_dis` there

--- crates/rspirv2-examples/src/bin/debug_dis.rs
+++ crates/rspirv2-examples/src/bin/debug_dis.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
+use rspirv2::core::inst_set::CoreInstSet;
+use rspirv2::inst::InstEncoding;
 use rspirv2::module::Module;
-use rspirv2_types::inst::InstEncoding;
 use std::path::PathBuf;
 
 #[derive(Clone, Debug, Parser)]
@@ -15,7 +16,7 @@ pub fn main() -> anyhow::Result<()> {
     let module = Module::from_bytes(binary.as_slice())?;
     let mut module_reader = module.reader();
     while let Some(mut inst) = module_reader.next()? {
-        let inst = rspirv2::core::inst_set::CoreInstSet::decode(&mut inst)?;
+        let inst = CoreInstSet::decode(&mut inst)?;
         println!("{:?}", inst);
     }
     Ok(())
