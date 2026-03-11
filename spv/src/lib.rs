2fa2f3ba81f7ec4327f2575df52fbe36217d70b7
firestar99 <firestar99@vectorware.com>
2026-03-11 11:29:00 +0100

    dis: add `dis_reference.spv` to compare disassemblers

--- spv/src/lib.rs
+++ spv/src/lib.rs
@@ -2,6 +2,10 @@ use std::path::PathBuf;
 
 pub const SPV_FOLDER: &str = env!("CARGO_MANIFEST_DIR");
 
+pub fn spv_folder() -> PathBuf {
+    SPV_FOLDER.into()
+}
+
 pub fn spv(name: &str) -> PathBuf {
     PathBuf::from(format!("{SPV_FOLDER}/{name}.spv"))
 }
