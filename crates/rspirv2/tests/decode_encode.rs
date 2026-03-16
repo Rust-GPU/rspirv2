de4c10d65107c6299d2f35f8fcf41616e9869c6b
firestar99 <firestar99@sydow.cloud>
2026-03-16 19:53:41 +0100

    big endian: add roundtrip with big endian encoded spv module

--- crates/rspirv2/tests/decode_encode.rs
+++ crates/rspirv2/tests/decode_encode.rs
@@ -1,10 +1,24 @@
 use rspirv2::module::Module;
 use rspirv2_types::inst::InstEncoding;
+use rspirv2_types::module::SPIRV_MAGIC;
 use spv::spv;
 
 #[test]
 fn test_bla() -> anyhow::Result<()> {
     roundtrip_spv(&std::fs::read(spv("bla"))?)
+}
+
+#[test]
+fn test_bla_be() -> anyhow::Result<()> {
+    let vec = std::fs::read(spv("bla"))?;
+    let vec = vec
+        .as_chunks()
+        .0
+        .iter()
+        .flat_map(|a| u32::from_ne_bytes(*a).swap_bytes().to_ne_bytes())
+        .collect::<Vec<_>>();
+    assert_eq!(&vec[..4], &SPIRV_MAGIC.0.swap_bytes().to_le_bytes());
+    roundtrip_spv(&vec)
 }
 
 fn roundtrip_spv(spv: &[u8]) -> anyhow::Result<()> {
