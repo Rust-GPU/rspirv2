e37ac418a574a4597441600454c00da8162a45fe
firestar99 <firestar99@sydow.cloud>
2026-03-17 14:57:59 +0100

    dis: use crate `test_expect` to bless tests

--- crates/tools-gen/src/dis.rs
+++ crates/tools-gen/src/dis.rs
@@ -42,9 +42,9 @@ impl Args {
 #[cfg(test)]
 pub mod test {
     use super::*;
-    use crate::test::BLESS;
+    use expect_test::{ExpectFile, expect_file};
     use rspirv2::core::inst_set::CoreInstSet;
-    use spv::{spv, spv_folder};
+    use spv::spv;
     use std::io::stdout;
 
     #[test]
@@ -58,20 +58,29 @@ pub mod test {
 
     #[test]
     fn test_dis_reference_default() -> anyhow::Result<()> {
-        test_dis_reference("dis_reference.rspirv2", Like::Default)
+        test_dis_reference(
+            expect_file!["../../../spv/dis_reference.rspirv2"],
+            Like::Default,
+        )
     }
 
     #[test]
     fn test_dis_reference_rspirv() -> anyhow::Result<()> {
-        test_dis_reference("dis_reference.rspirv_like", Like::Rspirv)
+        test_dis_reference(
+            expect_file!["../../../spv/dis_reference.rspirv_like"],
+            Like::Rspirv,
+        )
     }
 
     #[test]
     fn test_dis_reference_spirv_tools() -> anyhow::Result<()> {
-        test_dis_reference("dis_reference.spirv_tools_like", Like::SpirvTools)
+        test_dis_reference(
+            expect_file!["../../../spv/dis_reference.spirv_tools_like"],
+            Like::SpirvTools,
+        )
     }
 
-    fn test_dis_reference(ref_path: &str, like: Like) -> anyhow::Result<()> {
+    fn test_dis_reference(expect: ExpectFile, like: Like) -> anyhow::Result<()> {
         let args = Args {
             path: spv("dis_reference"),
             like,
@@ -79,16 +88,7 @@ pub mod test {
         let mut stdout = Vec::new();
         args.run::<CoreInstSet>(&mut stdout)?;
         let stdout = String::from_utf8(stdout)?;
-        let ref_path = spv_folder().join(ref_path);
-        if BLESS {
-            std::fs::write(&ref_path, &stdout)?;
-        }
-        let reference = std::fs::read_to_string(ref_path)?;
-        assert_eq!(
-            reference, stdout,
-            "`dis_reference.rspirv` and `rspirv2-dis dis_reference.spv` don't match, please run \
-            `cargo dis ./dis/dis_reference.spv &> ./dis/dis_reference.rspirv2`"
-        );
+        expect.assert_eq(&stdout);
         Ok(())
     }
 }
