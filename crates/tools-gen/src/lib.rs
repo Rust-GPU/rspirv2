58e8e669403c7664c70c53bc226826f6370ff415
firestar99 <firestar99@sydow.cloud>
2026-03-27 19:23:15 +0100

    tracy: add `tracy` profiling feature with heap alloc tracking

--- crates/tools-gen/src/lib.rs
+++ crates/tools-gen/src/lib.rs
@@ -1,1 +1,6 @@
 pub mod dis;
+
+#[cfg(feature = "tracy")]
+#[global_allocator]
+static GLOBAL: profiling::tracy_client::ProfiledAllocator<std::alloc::System> =
+    profiling::tracy_client::ProfiledAllocator::new(std::alloc::System, 100);
