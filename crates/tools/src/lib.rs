189511083a36516aa98b3bc363ed7dbbed6345c1
firestar99 <firestar99@sydow.cloud>
2026-07-15 14:01:05 +0200

    release: configure Cargo.toml for release

--- crates/tools/src/lib.rs
+++ crates/tools/src/lib.rs
@@ -1,1 +1,3 @@
+#![doc = include_str!("../README.md")]
+
 pub type ToolsISA = rspirv2::core::inst_set::CoreInstSet;
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,3 +1,5 @@
+#![doc = include_str!("../README.md")]
+
 use expect_test::ExpectFile;
 use std::path::PathBuf;
 
